e6f722a68cef14c431bdd564543ecffcc9f14ac3
Damhan91 <damhanp@gmail.com>
2021-08-23 14:14:38 +0000

    fixed question error

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,33 +2,32 @@ document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function(){
-            if (this.getAttribute("data-type") === "submit"){
-                alert ("You clicked Submit!");
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
             } else {
-                let gametype = this.getAttribute("data-type");
+                let gameType = this.getAttribute("data-type");
                 runGame(gameType);
             }
         });
     }
 
     runGame("addition");
+
 });
 
-
 function runGame(gameType) {
- let num1 = Math.floor (Math.random() * 25 + 1);
- let num2 = Math.floor (Math.random() * 25 + 1);
+    let num1 = Math.floor(Math.random() * 25 + 1);
+    let num2 = Math.floor(Math.random() * 25 + 1);
 
- if(gameType === "addition"){
-    displayAddtionQuestion (num1, num2);
- } else {
-     alert(`Unkown game type: ${gamestype}`);
-     throw `Unkown game type: ${gamestype}: aborting`;
- }
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
-
-function checkAnswer () {
+function checkAnswer() {
 
 }
 
@@ -40,20 +39,20 @@ function incrementScore() {
 
 }
 
-function incrementWrongAnswer (operand1, operand2) {
-    document.getElementById("operand1").textContent=operand1;
-    document.getElementById("operator").textContent="+";
-    document.getElementById("operand2").textContent=operand2;
+function incrementWrongAnswer() {
 }
 
-function displayAddtionQuestion () {
+function displayAdditionQuestion(operand1, operand2) {
 
-}
-
-function displaySubtractQuestion () {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
     
 }
+function displaySubtractQuestion() {
 
-function displayMultiplyQuestion () {
-    
+}
+
+function displayMultiplyQuestion() {
+
 }
