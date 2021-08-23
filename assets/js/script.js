8648d90e837f7f8c09797223bdfc8b6247332c84
Damhan91 <damhanp@gmail.com>
2021-08-23 11:46:38 +0000

    set up structure in javascript with basic functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer () {
+
+}
+
+function calculateCorrectrAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer () {
+
+}
+
+function displayAddtionQuestion () {
+
+}
+
+function displaySubtractQuestion () {
+    
+}
+
+function displayMultiplyQuestion () {
+    
+}
